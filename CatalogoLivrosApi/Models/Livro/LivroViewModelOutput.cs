6f39301100db64a17e904f01005078b082bd2dd0
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-25 09:30:50 -0300

    add LivroViewModelOutput.cs

---
+++ CatalogoLivrosApi/Models/Livro/LivroViewModelOutput.cs
@@ -0,0 +1,11 @@
+namespace CatalogoLivrosApi.Models.Livro
+{
+    public class LivroViewModelOutput
+    {
+        public string Titulo { get; set; }
+        public string Isbn { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+        public string Sinopse { get; set; } 
+    }
+}
